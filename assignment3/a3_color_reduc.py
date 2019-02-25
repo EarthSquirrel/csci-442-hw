9eab1593b7b353b72255a3ea5c46b9b60627b99c
Henry Soule <hsoule427@gmail.com>
2019-02-24 18:38:22 -0700

    Nothing to see here move along

--- assignment3/a3_color_reduc.py
+++ assignment3/a3_color_reduc.py
@@ -7,20 +7,26 @@ if sys_pf == 'darwin':
     import matplotlib
     matplotlib.use("TkAgg")
 
-n_colors = 10
-
-img_orig = cv.imread('./imagesWOvideo/one.jpg', cv.IMREAD_COLOR)
-
-arr = img_orig.reshape((-1,3))
-kmeans = KMeans(n_clusters=n_colors, random_state=42).fit(arr)
-labels = kmeans.labels_
-centers = kmeans.cluster_centers_
-less_colors = centers[labels].reshape(img_orig.shape).astype('uint8')
-
+def reduce_colors(img_orig, n_colors):
+	arr = img_orig.reshape((-1,3))
+	kmeans = KMeans(n_clusters=n_colors, random_state=42).fit(arr)
+	labels = kmeans.labels_
+	centers = kmeans.cluster_centers_
+	less_colors = centers[labels].reshape(img_orig.shape).astype('uint8')
+	cv.imshow('Color Reduction', less_colors)
+	cv.imwrite('10_color_reduc.jpg', less_colors)
 
 
-cv.imshow('Color Reduction', less_colors)
-cv.imwrite('10_color_reduc.jpg', less_colors)
+img = cv.imread('./10_color_reduc.jpg', cv.IMREAD_COLOR)
+blur = cv.blur(img,(3,3))
+edges = cv.Canny(blur,10,300)
+ret, thresh = cv.threshold(edges,0,255,0)
+img2, contours, hierarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+cv.drawContours(edges, contours, -1, (0,255,0), 3)
+cv.imshow('Edges', edges)
+
+
+
 
 
 cv.waitKey(0)
