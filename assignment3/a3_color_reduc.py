eb2483a207bb5cfa2c07869ea4fb7dcf09d9baa1
Henry Soule <hsoule427@gmail.com>
2019-02-26 19:47:49 -0700

    Tried new color_reduction formula

--- assignment3/a3_color_reduc.py
+++ assignment3/a3_color_reduc.py
@@ -3,6 +3,7 @@ import numpy as np
 from sys import platform as sys_pf
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 if sys_pf == 'darwin':
     import matplotlib
     matplotlib.use("TkAgg")
@@ -18,11 +19,26 @@ def reduce_colors(img_orig, n_colors, filename):
 	return less_colors
 
 
+def _reduce_colors(img_orig, n_clusters):
+	(h,w) = img_orig.shape[:2]
+	# image = cv.cvtColor(img_orig, cv.COLOR_BGR2LAB)
+	image = img_orig.reshape((img_orig.shape[0] * img_orig.shape[1],3))
+	clt = MiniBatchKMeans(n_clusters)
+	labels = clt.fit_predict(image)
+	quant = clt.cluster_centers_.astype("uint8")[labels]
+	quant = quant.reshape((h, w, 3))
+	image = image.reshape((h, w, 3))
+	# quant = cv.cvtColor(quant, cv.COLOR_LAB2BGR)
+	# image = cv.cvtColor(image, cv.COLOR_LAB2BGR)
+	cv.imshow("image", image)
+	cv.imshow("quant", quant)
+
+
 img = cv.imread('./imagesWOvideo/three.jpg', cv.IMREAD_COLOR)
-color_reduc = reduce_colors(img, 5, 'five_color_reduc_im3')
-cv.imshow("Color reduction", color_reduc)
+# color_reduc = reduce_colors(img, 5, 'five_color_reduc_im3')
+# cv.imshow("Color reduction", color_reduc)
 
-
+_reduce_colors(img, 4)
 
 
 
