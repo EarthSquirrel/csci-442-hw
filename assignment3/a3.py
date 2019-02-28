f9069240f03ba33b42d8c44f6847233e8e027f02
Britney <britney.gibbs@student.montana.edu>
2019-02-28 12:59:27 -0700

    Fixed text printing

--- assignment3/a3.py
+++ assignment3/a3.py
@@ -5,18 +5,9 @@ from matplotlib import pyplot as plt
 # from colormap import rgb2hex
 cv.namedWindow("Color circles")
 
-color_labels = {int('FF0000', 16): 'blue', int('00FF00', 16): 'green',
-                int('0000FF', 16): 'red', int('00FFFF', 16): 'yellow',
-                int('2a2aa5', 16): 'brown', int('99A5FF', 16): 'orange'}
 color_rgb = {'blue': [255, 0, 0], 'green': [0, 255, 0], 'red': [0, 0, 255],
              'yellow': [0, 255, 255], 'brown': [42, 42, 165], 'orange':
              [0, 165, 255]}
-def get_hsv_val(event, x, y, flags, param):
-    if event == cv.EVENT_LBUTTONDOWN:
-        vals = img[y][x]
-        print("colors are: ", vals)
-
-cv.setMouseCallback("Color circles", get_hsv_val)
 
 def get_label(rgb):
     min_dist = np.Infinity
@@ -32,8 +23,10 @@ def get_label(rgb):
     return label
 
 img = cv.imread('./imagesWOvideo/one.jpg', cv.IMREAD_COLOR)
-# img = cv.imread('./imagesWOvideo/two.jpg', cv.IMREAD_COLOR)
-thresh = img.copy()
+img = cv.imread('./imagesWOvideo/two.jpg', cv.IMREAD_COLOR)
+img = cv.imread('./imagesWOvideo/three.jpg', cv.IMREAD_COLOR)
+img = cv.imread('./imagesWOvideo/four.jpg', cv.IMREAD_COLOR)
+
 
 # Set up parameters for blob detection
 params = cv.SimpleBlobDetector_Params()
@@ -58,19 +51,8 @@ img_dilation = cv.dilate(img_erosion, kernel, iterations=1)
 
 edges = cv.Canny(img_dilation, 10, 300)
 # cv.imshow('edge', edges)
-thresh = edges.copy()
-img2,contours,hierarchy = cv.findContours(edges, 1, 2)
 
 
-# cnt = contours[0]
-# M = cv.moments(cnt)
-# print(M)
-# cv.drawContours(edges, contours, -1, (20,20,20), -1)
-# cv.imshow('edge2contour', edges)
-for cnt in contours:
-    x,y,w,h = cv.boundingRect(cnt)
-    cv.rectangle(thresh, (x,y), (x+w,y+h,), (255,255,0), 2)
-# cv.imshow('cont', thresh)
 
 """
 # did better without dilation of edges
@@ -95,11 +77,9 @@ keypoints_im = cv.drawKeypoints(edges, keypoints, np.array([]), (0, 255, 0),
 
 cv.imshow('Keypoints', keypoints_im)
 
-def rgb2hex(rgb):
-    return "{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
 
-def hex2rgb(hexcode):
-    return tuple(map(ord,hexcode[1:].decode('hex')))
+color_counts = {'red': 0, 'green': 0, 'blue': 0, 'yellow': 0, 'brown':0,
+                'orange': 0}
 
 # Draw new smaller circles
 width, height, channel = img.shape
@@ -107,7 +87,6 @@ white_img = 0* np.ones((width, height, 1), np.uint8)
 color_img = 0* np.ones((width, height, 1), np.uint8)
 color_img = cv.cvtColor(color_img, cv.COLOR_GRAY2RGB)
 
-colors = []
 for k in keypoints:
     tmp = 0* np.ones((width, height, 1), np.uint8)
     (x, y) = k.pt
@@ -117,30 +96,14 @@ for k in keypoints:
     cv.circle(tmp, (int(x), int(y)), 0, (255, 255, 255), radius)
     mc = cv.mean(img,mask = tmp)
     mc = [int(m) for m in mc]
-    colors.append(mc)
-    # print(mc)
     label = get_label(mc)
-    print(label)
     mc2 = mc.copy()
     mc = color_rgb[label]
-    print(mc2)
-    print(mc)
-    tmp = cv.cvtColor(tmp, cv.COLOR_GRAY2RGB)
-    cv.circle(tmp, (int(x), int(y)), 0, (mc[0], mc[1], mc[2]), radius)
-    ### cv.imshow('tmp {}'.format(label), tmp)
     cv.circle(color_img, (int(x), int(y)), 0, (mc[0], mc[1], mc[2]), radius)
-    cv.putText(color_img, str(mc2), (int(x),int(y)), cv.FONT_HERSHEY_SIMPLEX,.3,
-              (0,255,0),lineType=cv.LINE_AA)
 
-colors.sort()
-# print(colors)
-#for c in colors:
-#    print(rgb2hex(c))
 
 ### cv.imshow('White Cirlces', white_img)
 cv.imshow('Color circles', color_img)
-lab_img = cv.cvtColor(color_img, cv.COLOR_RGB2Lab)
-# cv.imshow('labels', lab_img)
 
 x_text = int(width) - 50
 y_text = int(height) - 10
@@ -150,22 +113,5 @@ cv.putText(img,output, (10, y_text), cv.FONT_HERSHEY_SIMPLEX,1,
            (255,255,255),2,cv.LINE_AA)
 
 cv.imshow('image', img)
-
-# get contures of white circles
-thresh = white_img.copy()
-img2,contours,hierarchy = cv.findContours(white_img, 1, 2)
-
-
-# cnt = contours[0]
-# M = cv.moments(cnt)
-# print(M)
-# cv.drawContours(white_img, contours, -1, (20,20,20), -1)
-# cv.imshow('edge2contour', white_img)
-
-for cnt in contours:
-    x,y,w,h = cv.boundingRect(cnt)
-    cv.rectangle(thresh, (x,y), (x+w,y+h,), (50,100,200), 2)
-# cv.imshow('cont', thresh)
-
 
 cv.waitKey(0)
