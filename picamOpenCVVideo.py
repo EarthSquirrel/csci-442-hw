2631a6d02df8d1aa8da61dde4b229bea72266cb7
henry-britney <britney.gibbs@student.montana.edu>
2019-03-09 20:21:32 -0700

    Edge detection hsv color filter for ORANGE!

--- picamOpenCVVideo.py
+++ picamOpenCVVideo.py
@@ -20,6 +20,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     img = frame.array
+    # __,new_image = cv.threshold(img, 0, 400, cv.THRESH_BINARY)
+    # cv.imshow("newimg", new_image)
     blur = cv.blur(img,(3,3))
 
     kernel = np.ones((5,5), np.uint8)
@@ -27,24 +29,29 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     img_erosion = cv.erode(blur, kernel, iterations=1)
     img_dilation = cv.dilate(img_erosion, kernel, iterations=1)
     # color filtering stuff, save for later
-    edges = cv.Canny(img_dilation, 35, 150, L2gradient=True)
+    hsv = cv.cvtColor(img_dilation, cv.COLOR_BGR2HSV)
+    # hsv = img_dilation.copy()
+
+    hsv_min, hsv_max = (0, 40, 90), (75, 200, 255)
+    color_filter = cv.inRange(hsv, hsv_min, hsv_max)
+    # pic = cv.Canny(hsv, 150, 170)
+
+    cv.imshow('hsv',color_filter) # hsv)
+
+    edges = cv.Canny(color_filter, 35, 150, L2gradient=True)
+    cv.imshow('edges', edges)
 
     contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     width, height, channel = img.shape
     tours = 255 * np.ones((width,height,1), np.uint8)
+    thresh = img.copy()
+    for cnt in contours:
+        x,y,w,h = cv.boundingRect(cnt)
+        cv.rectangle(thresh, (x,y), (x+w,y+h,), (0,255,0), 2)
 
     cv.drawContours(tours, contours, -1, (0,0,255), -1)
-    cv.imshow('edges', tours)
+    #  cv.imshow('edges', thresh)
 
-    """
-    hsv = cv.cvtColor(img_dilation, cv.COLOR_BGR2HSV)
-
-    hsv_min, hsv_max = (0, 50, 50), (100, 250, 255)
-    color_filter = cv.inRange(img_dilation, hsv_min, hsv_max)
-    # pic = cv.Canny(hsv, 150, 170)
-
-    cv.imshow('hsv', hsv) # color_filter) # hsv)
-    """
 
 
 
