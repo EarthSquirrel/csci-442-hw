cb223d778f6b1ed9514889906523d0809718e3c6
henry-britney <britney.gibbs@student.montana.edu>
2019-03-09 20:51:24 -0700

    Made screen smaller, fewer contors

--- picamOpenCVVideo.py
+++ picamOpenCVVideo.py
@@ -8,7 +8,7 @@ import numpy as np
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
-camera.framerate = 32
+camera.framerate = 15  #   32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # allow the camera to warmup
@@ -20,23 +20,25 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     img = frame.array
+    width, height, channel = img.shape
+    img = img[int(1*height/3):height, 20:width-20]
     # __,new_image = cv.threshold(img, 0, 400, cv.THRESH_BINARY)
     # cv.imshow("newimg", new_image)
-    blur = cv.blur(img,(3,3))
+    blur = cv.blur(img,(7,7))
 
-    kernel = np.ones((5,5), np.uint8)
+    kernel = np.ones((10,10), np.uint8)
 
-    img_erosion = cv.erode(blur, kernel, iterations=1)
-    img_dilation = cv.dilate(img_erosion, kernel, iterations=1)
+    img_erosion = cv.erode(blur, kernel, iterations=2)
+    img_dilation = cv.dilate(img_erosion, kernel, iterations=3)
     # color filtering stuff, save for later
     hsv = cv.cvtColor(img_dilation, cv.COLOR_BGR2HSV)
     # hsv = img_dilation.copy()
 
-    hsv_min, hsv_max = (0, 40, 90), (75, 200, 255)
+    hsv_min, hsv_max = (0, 40, 90), (75, 250, 255)
     color_filter = cv.inRange(hsv, hsv_min, hsv_max)
     # pic = cv.Canny(hsv, 150, 170)
 
-    cv.imshow('hsv',color_filter) # hsv)
+    # cv.imshow('hsv',color_filter) # hsv)
 
     edges = cv.Canny(color_filter, 35, 150, L2gradient=True)
     cv.imshow('edges', edges)
@@ -45,12 +47,17 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     width, height, channel = img.shape
     tours = 255 * np.ones((width,height,1), np.uint8)
     thresh = img.copy()
+
+    cntsSorted = sorted(contours, key=lambda x: cv.contourArea(x))
+    # print(len(contours))
+    # contours = cntsSorted[-3]
+
     for cnt in contours:
         x,y,w,h = cv.boundingRect(cnt)
         cv.rectangle(thresh, (x,y), (x+w,y+h,), (0,255,0), 2)
 
     cv.drawContours(tours, contours, -1, (0,0,255), -1)
-    #  cv.imshow('edges', thresh)
+    cv.imshow('Contours', thresh)
 
 
 
