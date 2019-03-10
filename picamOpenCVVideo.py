97f2fd0486d7018887aef68bc12d74909ba93dc1
henry-britney <britney.gibbs@student.montana.edu>
2019-03-09 17:37:35 -0700

    Changed cv2 to cv

--- picamOpenCVVideo.py
+++ picamOpenCVVideo.py
@@ -2,7 +2,7 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-import cv2
+import cv2 as cv
 import numpy as np
 
 # initialize the camera and grab a reference to the raw camera capture
@@ -20,18 +20,34 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # and occupied/unoccupied text
     img = frame.array
 
-    blur = cv2.blur(img,(3,3))
+    blur = cv.blur(img,(3,3))
 
     kernel = np.ones((5,5), np.uint8)
 
-    img_erosion = cv2.erode(blur, kernel, iterations=1)
-    img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
+    img_erosion = cv.erode(blur, kernel, iterations=1)
+    img_dilation = cv.dilate(img_erosion, kernel, iterations=1)
 
 
-    pic = cv2.Canny(img_dilation, 150, 170)
+    pic = cv.Canny(img_dilation, 150, 170)
+
+    params = cv.SimpleBlobDetector_Params()
+
+
+    # Change thresholds
+    params.minThreshold = 2# 2
+    params.maxThreshold = 500
+    params.filterByCircularity = True
+    params.minCircularity = 0.3
+    params.maxCircularity = 1.5
+
+
+    detector = cv.SimpleBlobDetector_create(params)
+
+    keypoints = detector.detect(edges)
+
     # show the frame
-    cv2.imshow("Frame", pic)
-    key = cv2.waitKey(1) & 0xFF
+    cv.imshow("Frame", pic)
+    key = cv.waitKey(1) & 0xFF
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
