052ea37d5ee1440ce5e58e0fdd82a916ff2f001b
Henry Soule <hsoule427@gmail.com>
2019-03-10 19:20:13 -0600

    wrote a quit function

--- follow_line.py
+++ follow_line.py
@@ -1,3 +1,18 @@
 import cv2 as cv
+import tkinter as tk
+import maestro
+import numpy as np
+# import keyboardControl as kc
+def quit(event):
+	global win
+	win.destroy()
+
+win = tk.Tk()
+
+
+win.bind('<q>', quit)
+win.mainloop()
+# keys = kc.KeyControl(win)
+
 
 
