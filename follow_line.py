be7e25f5b3c29543f770d71aef9b4a5eed4fa0c5
Henry Soule <hsoule427@gmail.com>
2019-03-10 22:08:40 -0600

    Small changes

--- follow_line.py
+++ follow_line.py
@@ -7,12 +7,22 @@ def quit(event):
 	global win
 	win.destroy()
 
+
+def arrow(key):
+	print(key.keycode)
+	print('\n')
+
 win = tk.Tk()
-
-
+win.bind('<Up>', arrow)
+win.bind('<Left>', arrow)
+win.bind('<Down>', arrow)
+win.bind('<Right>', arrow)
+win.bind('<space>', arrow)
 win.bind('<q>', quit)
 win.mainloop()
 # keys = kc.KeyControl(win)
+
+
 
 
 
