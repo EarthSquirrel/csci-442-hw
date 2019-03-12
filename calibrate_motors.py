5d36a1178dcd7af43b7de8883e9e4c783c6a25df
Henry Soule <hsoule427@gmail.com>
2019-03-12 17:53:39 -0600

    Made some edits

--- calibrate_motors.py
+++ calibrate_motors.py
@@ -1,6 +1,7 @@
 import time
 import cv2 as cv
 import numpy as np
+import maestro
 
 MOTORS = 1
 TURN = 2
@@ -13,32 +14,34 @@ max_move = 5400
 max_turn = 6600  # right
 min_turn = 5400  # left
 
-servo = maestro.controller()
+servo = maestro.Controller()
 
 def start_motors():
 	global motors
-    while motors > max_move - 200:
-        motors -= 300
-        servo.setTarget(MOTORS, motors)
-        time.sleep(0.01)
+	while motors > max_move - 200:
+	    motors -= 300
+	    servo.setTarget(MOTORS, motors)
+	    time.sleep(0.01)
 
 def turn_left():
 	global motors, turn
-    print('left')
-    while turn > min_turn:
-    	turn -= 200
-    	servo.setTarget(TURN, turn)
+	print('left')
+	while turn > min_turn:
+		turn -= 200
+		servo.setTarget(TURN, turn)
 
 def turn_right():
 	global motors, turn
-    print('left')
-    while turn < max_turn:
-    	turn += 200
-    	servo.setTarget(TURN, turn)
+	print('left')
+	while turn < max_turn:
+		turn += 200
+		servo.setTarget(TURN, turn)
 
 
 def go_straight():
-
+	global motors, turn
+	servo.setTarget(TURN, 6000)
+	servo.setTarget(MOTORS, max_move)
 
 
 def slow_down():
@@ -47,8 +50,26 @@ def slow_down():
 		motors -= 200
 
 def stop():
-    servo.setTarget(TURN, 6000)
-    servo.setTarget(MOTORS, 6000)
+	servo.setTarget(TURN, 6000)
+	servo.setTarget(MOTORS, 6000)
+
+
+try:
+	time.sleep(1)
+	start_motors()
+	time.sleep(2)
+	slow_down()
+	turn_left()
+	turn_left()
+	go_straight()
+
+except: # catch *all* exceptions
+    e = sys.exc_info()
+    print(e)
+    # keys.arrow (65)
+    stop()
+    print('Stopped motors due to an error')
+
 
 
 
