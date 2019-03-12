536b505f54fa05835ab96284acd80069ba9f8fbb
henry-britney <britney.gibbs@student.montana.edu>
2019-03-12 17:54:45 -0600

    changes

--- follow_line.py
+++ follow_line.py
@@ -1,1 +1,15 @@
 import cv2 as cv
+import tkinter as tk
+import maestro
+import keyboardControl
+
+keys = keyboardControl.KeyControl()
+servo = maestro.Controller()
+#servo.setAccel(0,4)
+#servo.setTarget(0, 0)# ,6000)
+#servo.setSpeed(0,5)
+#x = servo.getPosition(1)
+#print(x)
+
+servo.close()
+
