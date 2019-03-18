2fb3a5b151b0971ea7f21b7faea28b5edfa98095
henry-britney <britney.gibbs@student.montana.edu>
2019-03-18 17:29:10 -0600

    Can detect face head on only!

--- greet_human.py
+++ greet_human.py
@@ -2,29 +2,33 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-import cv2
+import cv2 as cv
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
-camera.framerate = 32
+camera.framerate = 10
 rawCapture = PiRGBArray(camera, size=(640, 480))
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+
 
 def faces_found(frame):
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     real_faces = []
+    # if len(faces) > 0:
+        # print('Faces total found: {}'.format(len(faces)))
     for (x,y,w,h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
         # If there are no eyes, don't use this face
         if len(eyes) == 0:
             continue
         #Otherwise, we use it and return it to our list of return faces
+        # print('total eye count: {}'.format(len(eyes)))
         real_faces.append((x,y,w,h))
-        cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        for (ex,ey,ew,eh) in eyes:
-            cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        # cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
     return real_faces
 
 # allow the camera to warmup
@@ -35,10 +39,17 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-    pic = cv2.Canny(image, 100, 170)
-    # show the frame
-    cv2.imshow("Frame", pic)
-    key = cv2.waitKey(1) & 0xFF
+
+    # TODO: Scanning code here!
+
+    # run facial recognition on image
+    faces = faces_found(image)
+    for (x,y,w,h) in faces:
+        print('found a face!')
+        cv.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+
+    cv.imshow("Frame", image)
+    key = cv.waitKey(1) & 0xFF
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
