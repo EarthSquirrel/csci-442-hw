630945fee5413ff8a3ea32efadc40edf60ea0161
Britney <britney.gibbs@student.montana.edu>
2019-04-03 08:28:21 -0600

    Added stuff

--- testTimers.py
+++ testTimers.py
@@ -7,11 +7,17 @@ def foo():
     global val
     val += 1
     print(time.ctime(), ' ', val)
-    threading.Timer(2, foo).start()
+    # threading.Timer(2, foo).start()
 
 foo()
+test = threading.Timer(2, foo)
+test.start()
 
 for x in range(10):
     time.sleep(5)
     val = 0
     print('for loop val ', val)
+
+    if x == 4:
+        print('stoping test')
+        test.cancel()
