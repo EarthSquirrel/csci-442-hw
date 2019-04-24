93c6a2da5f45a8a955075b584ed9164530f05f46
Henry Soule <hsoule427@gmail.com>
2019-04-24 17:53:51 -0600

    Just switched the images

--- get_hsv_vals.py
+++ get_hsv_vals.py
@@ -4,10 +4,12 @@ import cv2 as cv
 cv.namedWindow("original", cv.WINDOW_NORMAL)
 cv.namedWindow("hsv", cv.WINDOW_NORMAL)
 cv.namedWindow("filter", cv.WINDOW_NORMAL)
+cv.namedWindow("hsv ice img", cv.WINDOW_NORMAL)
 
 img = cv.imread('image-.png')# 'hand-imag2e.png')
-ice_img = cv.imread('ice_image.png')
-hsv_ice_img = cv.cvtColor(ice_img, cv.COLOR_BGR2HSV)
+yellow_img = cv.imread('yellow.png')
+green_img = cv.imread('green.png')
+hsv_ice_img = cv.cvtColor(green_img, cv.COLOR_BGR2HSV)
 hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
 
@@ -25,7 +27,13 @@ white_max = np.array([130, 20, 255])
 pink_min = np.array([164, 65, 252])
 pink_max = np.array([166, 75, 255])
 
-hsv_filter = cv.inRange(hsv_img, pink_min, pink_max)
+yellow_ice_min = np.array([30, 135, 200])
+yellow_ice_max = np.array([35, 170, 220])
+
+green_ice_min = np.array([50, 190, 160])
+green_ice_max = np.array([55, 205, 170])
+
+hsv_filter = cv.inRange(hsv_ice_img, green_ice_min, green_ice_max)
 
 # worked on hand-imag2e.png
 # yellow_min = np.array([100, 0, 150])
@@ -33,20 +41,18 @@ hsv_filter = cv.inRange(hsv_img, pink_min, pink_max)
 
 
 def get_hsv_val(event, x, y, flags, param):
-    global hsv_img
+    global hsv_ice_img
     if event == cv.EVENT_LBUTTONDOWN:
         img2 = hsv_ice_img
         vals = img2[y][x]
         print("Vals: ", vals)
 
 
-cv.setMouseCallback("hsv", get_hsv_val)
+cv.setMouseCallback("hsv ice img", get_hsv_val)
 
 while True:
-	cv.imshow('original', img)
-	cv.imshow('ice image', ice_img)
-	cv.imshow('hsv ice image', hsv_ice_img)
-	cv.imshow('hsv', hsv_img)
+	cv.imshow('original', green_img)
+	cv.imshow('hsv ice img', hsv_ice_img)
 	cv.imshow('filter', hsv_filter)
 
 	k = cv.waitKey(1)
